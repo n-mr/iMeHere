0beb819ebcad0c194889e044729cb442581cb991
n-mr <101756651+n-mr@users.noreply.github.com>
2022-06-11 02:36:42 +0300

    Hi

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    new Typed('.firstText', {
+        strings: ["Hi and welcome,  I'm v2Talal 👋"],
+        typeSpeed: 25,
+        startDelay: 900,
+        showCursor: false
+      });
+    }),
+    new Typed('.personalInfo', {
+        strings: ["From Kuwait, I Love My Just Pc, I'm Joking Joking i love all 😅", " اهلا وسهلا ومرحباا", "ماتعرفني صح انا اعرفك انت اهو انت صح ؟", "From Kuwait, I Love My Just Pc, I'm Joking Joking i love all 😅"],
+        typeSpeed: 35,
+        startDelay: 2000,
+        showCursor: false
+    }),
+    new Typed('.mediaInfoStart', {
+        strings: ["You can check out my - ضيفني لا تنسى بالبرامج الثانية "],
+        typeSpeed: 25,
+        startDelay: 5000,
+        showCursor: false
+    }),
+    new Typed('.mediaInfoEnd', {
+        strings: ["By clicking the icon down below - اضغط الايقونه الي تحت وضيفني"],
+        typeSpeed: 23,
+        startDelay: 7000,
+        showCursor: false
+    }),
+    new Typed('.mailInfo', {
+        strings: ["if you want contact at my email:  <a href='mailto:me@Talal.pro'>me@Talal.pro</a><span class='cursor'>_</span>"],
+        typeSpeed: 25,
+        startDelay: 9500,
+        showCursor: false
+    });
+
